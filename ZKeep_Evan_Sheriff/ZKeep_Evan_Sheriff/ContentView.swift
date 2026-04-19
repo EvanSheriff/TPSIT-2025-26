0f2e48aa10c78bdde62b945c55db32f8989423bc
EvanSheriff <157704463+EvanSheriff@users.noreply.github.com>
2026-04-19 21:55:58 +0200

    Add files via upload

---
+++ ZKeep_Evan_Sheriff/ZKeep_Evan_Sheriff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZKeep_Evan_Sheriff
+//
+//  Created by Evan on 18/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZKeep_Evan_Sheriff/ZKeep_Evan_Sheriff/ZKeep_Evan_SheriffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZKeep_Evan_SheriffApp.swift
+//  ZKeep_Evan_Sheriff
+//
+//  Created by Evan on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZKeep_Evan_SheriffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
